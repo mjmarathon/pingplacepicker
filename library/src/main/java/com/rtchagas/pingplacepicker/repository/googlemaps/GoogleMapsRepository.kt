055a025770e0208b11f2bfb3dd91db5fd3a7bc11
Michaeljon <mhayden@ourfamilywizard.com>
2019-07-08 15:55:17 -0500

    Revert: Set getPlaceById to fetch for POI clicks. (bf7c626b)

--- library/src/main/java/com/rtchagas/pingplacepicker/repository/googlemaps/GoogleMapsRepository.kt
+++ library/src/main/java/com/rtchagas/pingplacepicker/repository/googlemaps/GoogleMapsRepository.kt
@@ -102,7 +102,7 @@ class GoogleMapsRepository constructor(
     /**
      * This call to Google Places API is totally free :)
      */
-    override fun getPlaceById(placeId: String): Single<Place> {
+    private fun getPlaceById(placeId: String): Single<Place> {
 
         // Create the request
         val request = FetchPlaceRequest.builder(placeId, getPlaceFields()).build()
